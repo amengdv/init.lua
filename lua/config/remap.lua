c507c3e3bb0c92aaecad48e1393883565db2072b
amengdv <aminkafri0082@gmail.com>
2024-12-23 16:18:58 +0800

    no more map for clearing hl

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,7 +4,6 @@ vim.keymap.set('v', '<leader>x', ':lua<CR>')
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 vim.keymap.set('i', '<C-c>', '<Esc>');
-vim.keymap.set('n', '<C-c>', ':nohlsearch<CR>', { silent = true });
 
 vim.keymap.set('n', '<C-;>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', "<C-'>", '<cmd>cprev<CR>zz')
@@ -15,5 +14,5 @@ vim.keymap.set('v', 'K', ":m '<-2'<CR>gv=gv")
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
-vim.keymap.set('v', '<leader>y', [[ "+y ]])
-vim.keymap.set('n', '<leader>Y', [[ "+Y ]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
